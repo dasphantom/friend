b9e7ec6f9307fe32b8426f88619a268e68ac7e4c
dasphantom <mail@speeters.nl>
2018-02-24 18:49:20 +0100

    #update

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import sqlite3
 
 conn = sqlite3.connect('friend.db')
-
 c = conn.cursor()
-c.execute('select * from phrases')
-all_rows = c.fetchall()
-print(all_rows)
+
+#1. opening and getting first user respons
+print ("Hey")
+
+while True:
+    user_input = input()
+
+    #2. check if user input already exists and print possible output
+    c.execute('''SELECT output FROM phrases
+          WHERE input=?'''
+          ,(user_input,))
+
+    row = c.fetchone()
+
+    try:
+        print(row[0])
+
+    except (TypeError):
+        print("weird")
+
+
 
 
 
