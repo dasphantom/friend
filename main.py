9ab08cc47023a63f5ea73fdc77bd8cc990a7d1f3
dasphantom <mail@speeters.nl>
2018-02-27 22:01:19 +0100

    #update

--- main.py
+++ main.py
@@ -9,27 +9,33 @@ print ("Hey")
 while True:
     user_input = input()
 
+    #terminate on request
+    if user_input.lower() == 'bye':
+        print("Cya")
+        quit()
+
+
     #2. check if user input already exists and print possible output
     c.execute('''SELECT output FROM phrases
-          WHERE input=?'''
-          ,(user_input,))
+          WHERE lower(input)=?'''
+          ,(user_input.lower(),))
 
     row = c.fetchone()
+
 
     try:
         print(row[0])
 
+    #3. nothing found, we need to ask the user for an appropriate response
     except (TypeError):
-        print("weird")
+        print("How to respond to that?")
 
+        user_input2 = input()
 
+        c.execute('''INSERT INTO phrases (input,output)
+        VALUES (?,?)'''
+        ,(user_input,user_input2))
 
+        conn.commit()
 
-
-
-
-
-
-
-
-
+        print("Thank you")
